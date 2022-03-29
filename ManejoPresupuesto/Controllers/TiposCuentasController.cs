68fefa7c2c0eb6b6f06ecd40dcaed458da6c2a52
laur73 <jj.c.r.1781@gmail.com>
2022-03-29 11:22:35 -0600

    Seccion 4 completada

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,159 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        //Metodo para listar los tipos cuentas
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            return View(tiposCuentas);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        //Metodo para crear un tipo cuenta en el formulario
+        [HttpPost]
+        public async Task<IActionResult> Crear(TipoCuentaViewModel tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            tipoCuenta.UsuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+
+            if (yaExisteTipoCuenta)
+            {
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre), $"El nombre {tipoCuenta.Nombre} ya existe");
+
+                return View(tipoCuenta);
+            }
+
+            await repositorioTiposCuentas.Crear(tipoCuenta);
+
+            return RedirectToAction("Index");
+        }
+
+        //Metodo para obtener el tipo cuenta de Id para poder asi editar
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(tipoCuenta);
+        }
+
+        //Método para editar un tipo cuenta
+        [HttpPost]
+        public async Task<IActionResult> Editar(TipoCuentaViewModel tipoCuenta)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuentaExiste = await repositorioTiposCuentas.ObtenerPorId(tipoCuenta.Id, usuarioId);
+            {
+                if (tipoCuentaExiste is null)
+                {
+                    return RedirectToAction("NoEncontrado", "Home");
+                }
+                await repositorioTiposCuentas.Actualizar(tipoCuenta);
+                return RedirectToAction("Index");
+            }
+        }
+
+        //Metodo para ver si esta el tipo cuenta a borrar
+        [HttpGet]
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                //("Vista", "Controlador")
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(tipoCuenta);
+        }
+
+        //Metodo para borrar una cuenta al postear
+        [HttpPost]
+        public async Task<IActionResult> BorrarTipoCuenta(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if(tipoCuenta is null)
+            {
+                return RedirectToAction("No encontrado", "Home");
+            }
+
+            await repositorioTiposCuentas.Borrar(id);
+            return RedirectToAction("Index");
+        }
+
+        //Metodo para devolver un error en caso de si ya existe un tipo cuenta al momento de crear uno nuevo
+        [HttpGet]
+        public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(nombre, usuarioId);
+
+            if (yaExisteTipoCuenta)
+            {
+                return Json($"El nombre {nombre} ya existe");
+            }
+
+            return Json(true);
+        }
+
+        //Método para guardar el orden de los tipos cuentas
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var idsTiposCuentas = tiposCuentas.Select(x => x.Id);
+
+            var idsTiposCuentasNoPertenecenAlUsuario = ids.Except(idsTiposCuentas).ToList();
+
+            if (idsTiposCuentasNoPertenecenAlUsuario.Count > 0)
+            {
+                return Forbid();
+            }
+
+            var tiposCuentasOrdenados = ids.Select((valor, indice) =>
+                new TipoCuentaViewModel() { Id = valor, Orden = indice + 1 }).AsEnumerable();
+
+            await repositorioTiposCuentas.Ordenar(tiposCuentasOrdenados);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,96 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+//Esta clase contiene los metodos de Crear, Leer, Actualizar y Borrar un tipo de cuenta
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        Task Actualizar(TipoCuentaViewModel tipoCuenta);
+        Task Borrar(int id);
+        Task Crear(TipoCuentaViewModel tipoCuenta);
+        Task<bool> Existe(string nombre, int usuarioId);
+        Task<IEnumerable<TipoCuentaViewModel>> Obtener(int usuarioId);
+        Task<TipoCuentaViewModel> ObtenerPorId(int id, int usuarioId);
+        Task Ordenar(IEnumerable<TipoCuentaViewModel> tipoCuentasOrdenados);
+    }
+
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        private readonly string connectionString;
+
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        //Metodo para poder crear un tipo cuenta en la BD
+
+        public async Task Crear(TipoCuentaViewModel tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"TiposCuentas_Insertar",
+                                                           new { usuarioId = tipoCuenta.UsuarioId, nombre = tipoCuenta.Nombre },
+                                                           commandType: System.Data.CommandType.StoredProcedure);
+            tipoCuenta.Id = id;
+        }
+
+        //Metodo para saber si existe un registro de tipos cuentas para no repetirlo
+
+        public async Task<bool> Existe(string nombre, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(@"SELECT 1
+                                                    FROM TiposCuentas
+                                                    WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId;", new { nombre, usuarioId });
+
+            return existe == 1;
+        }
+
+        //Metodo para listar los tipos de cuentas creados
+
+        public async Task<IEnumerable<TipoCuentaViewModel>> Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<TipoCuentaViewModel>(@"SELECT Id, Nombre, Orden
+                                                    FROM TiposCuentas
+                                                    WHERE UsuarioId = @usuarioId
+                                                    ORDER BY Orden;", new { usuarioId });
+        }
+
+        //Método para editar un tipo cuenta
+        public async Task Actualizar(TipoCuentaViewModel tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE TiposCuentas
+                                            SET Nombre = @Nombre
+                                            WHERE Id = @Id;", tipoCuenta);
+        }
+
+        //Método para obtener tipo cuenta por Id
+        public async Task<TipoCuentaViewModel> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<TipoCuentaViewModel>(@"SELECT Id, Nombre, Orden
+                                                    FROM TiposCuentas
+                                                    WHERE Id = @Id AND UsuarioId = @UsuarioId;", new { id, usuarioId });
+        }
+
+        //Método para borrar un tipo cuenta
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"DELETE TiposCuentas WHERE Id = @Id;", new { id });
+        }
+
+        //Método para ordenar los tipos cuentas
+        public async Task Ordenar(IEnumerable<TipoCuentaViewModel> tipoCuentasOrdenados)
+        {
+            var query = "UPDATE TiposCuentas SET Orden = @Orden WHERE Id = @Id;";
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(query, tipoCuentasOrdenados);
+        }
+    }
+}
